46d3ef3ac70661b9acbaf6b587a654cd869d9023
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-12-19 21:04:40 -0300

    docs: add docs

--- src/resources/sale/dto/create-sale.dto.ts
+++ src/resources/sale/dto/create-sale.dto.ts
@@ -1,21 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import { ArrayNotEmpty, IsArray, IsDate, IsMongoId, IsOptional, ValidateNested } from 'class-validator'
 import { ObjectId } from 'mongoose'
 import { ArticleDto } from './article.dto'
-
 export class CreateSaleDto {
 	@IsOptional()
 	@IsDate()
+	@ApiProperty({ example: '2021-09-01T00:00:00.000Z', description: 'Date', required: false, type: Date })
 	date: Date
 
 	@IsArray()
 	@ArrayNotEmpty()
 	@Type(() => ArticleDto)
 	@ValidateNested({ each: true })
+	@ApiProperty({ 
+		example: [{ 
+			article: '612f4d6a5c5d7d0015d8f6b8', quantity: 1, discount: 0 }], 
+			description: 'Articles', 
+			required: true, 
+			type: [ArticleDto] 
+	})
 	articles: ArticleDto[]
 
+	@ApiProperty({ example: 'Observations', description: 'Observations', required: true, type: String })
 	observations: string
 
+	@ApiProperty({ example: '612f4d6a5c5d7d0015d8f6b8', description: 'Client', required: true, type: String })
 	@IsMongoId()
 	client: ObjectId
 }
--- src/resources/user/dto/create-user.dto.ts
+++ src/resources/user/dto/create-user.dto.ts
@@ -1,7 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsOptional, IsStrongPassword, MinLength } from 'class-validator'
-
 export class CreateUserDto {
 	@IsEmail()
+	@ApiProperty({ example: 'hola@gmail.com', description: 'Email', required: true, type: String })
 	email: string
 
 	@IsStrongPassword({
@@ -11,12 +12,15 @@ export class CreateUserDto {
 		minLowercase: 0,
 		minUppercase: 0
 	})
+	@ApiProperty({ example: '12345678', description: 'Password', required: true, type: String })
 	password: string
 
 	@MinLength(2)
+	@ApiProperty({ example: 'hola', description: 'Username', required: true, type: String })
 	username: string
 
 	@IsOptional()
 	@MinLength(2)
+	@ApiProperty({ example: 'hola', description: 'Name', required: true, type: String })
 	name: string
 }
