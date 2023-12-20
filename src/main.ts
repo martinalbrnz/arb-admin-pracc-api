0f157391bceceaf3606f4bf3fcca5f1b1eb662fa
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-12-19 21:14:55 -0300

    fix: trash text

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('API')
+  .setDescription('ARB Practice')
   .setVersion('1.0')
-  .addTag('cats')
+  .addTag('ARB')
   .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
